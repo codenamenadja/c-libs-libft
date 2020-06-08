19bc6d505a3f11a6c1fb5598fa0f49037ee52775
junehan <junehan.dev@gmail.com>
2020-06-08 18:58:00 +0900

    ft_v1.h updates(new2 move1)

--- includes/ft_v1.h
+++ includes/ft_v1.h
@@ -1,10 +1,16 @@
 #ifndef __LIBFT_V1_H
 # define __LIBFT_V1_H
-void ft_putchar(char c);
-void ft_swap(int *a, int *b);
-void ft_putstr(char *str);
 int ft_strlen(char *str);
 int ft_strcmp(const char *s1, const char *s2);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
+
+void ft_putchar(char c);
+void ft_swap(int *a, int *b);
+void ft_putstr(char *str);
+
 char *ft_strchr(const char *s, int c);
+char *ft_strrchr(const char *s, int c);
+char *ft_strnchr(const char *s, int c, size_t n);
+
+char *ft_strdup(const char *s);
 #endif

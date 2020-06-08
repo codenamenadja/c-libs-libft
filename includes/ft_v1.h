b8cb055203478d1b75f16f569ea8b7f9ca9f1623
junehan <junehan.dev@gmail.com>
2020-06-08 18:10:55 +0900

    includes_ft_v1.h +2 added

--- includes/ft_v1.h
+++ includes/ft_v1.h
@@ -4,5 +4,7 @@ void ft_putchar(char c);
 void ft_swap(int *a, int *b);
 void ft_putstr(char *str);
 int ft_strlen(char *str);
-int ft_strcmp(char *s1, char *s2);
+int ft_strcmp(const char *s1, const char *s2);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
+char *ft_strchr(const char *s, int c);
 #endif

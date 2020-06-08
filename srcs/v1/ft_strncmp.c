4a7f98ed2140b8d7654c03dca16f772daacd25e2
junehan <junehan.dev@gmail.com>
2020-06-08 18:09:18 +0900

    src_v1_ft_strncmp new

---
+++ srcs/v1/ft_strncmp.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+int     ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+    size_t i;
+
+    if(!n)
+        return (0);
+    i = 0;
+    while ((*(s1 + i) == *(s2 + i)) && i < n)
+    {
+        if (*(s1 + i) == 0)
+            break;
+        i++;
+    }
+    return (*(s1 + i) - *(s2 + i));
+}

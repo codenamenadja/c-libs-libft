99afb6125ced0763fb38c8a825c66af631cc1210
junehan <junehan.dev@gmail.com>
2020-10-30 14:49:06 +0900

    ctype/ft_iseven added

--- includes/libft_ctype.h
+++ includes/libft_ctype.h
@@ -1,12 +1,13 @@
 #ifndef _FT_CTYPE
 #define _FT_CTYPE
-int isalnum(int c);
-int isalpha(int c);
-int isdigit(int c);
-int islower(int c);
-int isprint(int c);
-int isspace(int c);
-int isupper(int c);
-int isascii(int c);
-int isblank(int c);
+int ft_isalnum(int c);
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_islower(int c);
+int ft_isprint(int c);
+int ft_isspace(int c);
+int ft_isupper(int c);
+int ft_isascii(int c);
+int ft_isblank(int c);
+int ft_iseven(int c);
 #endif
